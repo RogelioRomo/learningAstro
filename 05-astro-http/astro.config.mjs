12a7ab245e63ffe8044947d373f10c7564b8b530
rogelioromo <rogelioromo25@gmail.com>
2025-06-10 20:16:35 -0700

    last commit

--- 05-astro-http/astro.config.mjs
+++ 05-astro-http/astro.config.mjs
@@ -6,14 +6,15 @@ import sitemap from '@astrojs/sitemap';
 
 import db from '@astrojs/db';
 
-// import node from '@astrojs/node';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), db()],
   output: 'hybrid',
-  // adapter: node({
-  //   mode: 'standalone',
-  // }),
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
--- 05-astro-http/db/seed.ts
+++ 05-astro-http/db/seed.ts
@@ -1,6 +1,15 @@
-import { db } from 'astro:db';
+import { Clients, db } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+  // TODO
+  await db.insert(Clients).values([
+    { id: 1, name: "Kasim", age: 35, isActive: true },
+    { id: 2, name: "Fernando", age: 32, isActive: true },
+    { id: 3, name: "Maria", age: 25, isActive: true },
+    { id: 4, name: "Carlos", age: 45, isActive: false },
+    { id: 5, name: "Emin", age: 18, isActive: true },
+  ])
+
+  console.log('Seed executed')
 }
